727d004574b61d24b5649de4e691afd09292b3f9
Andre Fuller <afuller611@gmail.com>
2020-04-09 13:28:37 -0600

    Italian should be good

--- src/components/CampaignAnalyze.js
+++ src/components/CampaignAnalyze.js
@@ -5,7 +5,26 @@ import { CheckCircle } from '@material-ui/icons'
 import '../custom.css'
 
 const currencies = ['USD', 'EUR', 'GBP', 'CAD', 'AUD', 'CHF', 'SEK', 'NOK', 'DKK']
-const categories = ['Accidents & Emergencies', 'Animals & Pets', 'Babies Kids & Family', 'Business & Entrepreneurs', 'Celebrations & Events', 'Community & Neighbors', 'Creative Arts Music & Film', 'Funerals & Memorials', 'Travel & Adventure', 'Medical Illness & Healing', 'Missions Faith & Church', 'Weddings & Honeymoons', 'Sports Teams & Clubs', 'Education & Learning', 'Volunteer & Service', 'Competitions & Pageants', 'Dreams Hopes & Wishes', 'Other']
+const categories = [
+    { name: 'Accidents & Emergencies', id: 2 },
+    { name: 'Animals & Pets', id: 3 },
+    { name: 'Babies Kids & Family', id: 4 },
+    { name: 'Business & Entrepreneurs', id: 5 },
+    { name: 'Celebrations & Events', id: 6 },
+    { name: 'Community & Neighbors', id: 7 },
+    { name: 'Creative Arts Music & Film', id: 8 },
+    { name: 'Funerals & Memorials', id: 9 },
+    { name: 'Travel & Adventure', id: 10 },
+    { name: 'Medical Illness & Healing', id: 11 },
+    { name: 'Missions Faith & Church', id: 12 },
+    { name: 'Weddings & Honeymoons', id: 14 },
+    { name: 'Sports Teams & Clubs', id: 16 },
+    { name: 'Education & Learning', id: 17 },
+    { name: 'Volunteer & Service', id: 18 },
+    { name: 'Competitions & Pageants', id: 19 },
+    { name: 'Dreams Hopes & Wishes', id: 20 },
+    { name: 'Other', id: 15 }
+]
 
 const CampaignAnalyze = (props) => {
     const [state, setState] = React.useState({
@@ -14,10 +33,12 @@ const CampaignAnalyze = (props) => {
         title: "",
         currencyCode: "",
         category: "",
+        categoryId: "",
         autoFbPost: false,
         isCharity: false,
         visibleInSearch: false,
         mediaType: 0,
+        hasBeneficiary: false
     })
 
 
@@ -27,27 +48,34 @@ const CampaignAnalyze = (props) => {
 
     const [result, setResult] = React.useState("")
 
+    const [error, setError] = React.useState(false)
+
     const handleSubmit = (e) => {
         setNotNumber(false)
         setResult("")
+        setError(false)
         e.preventDefault()
         if (isNaN((parseInt(state.goal)))) {
             setNotNumber(true)
             return
         }
         setLoading(true)
-        axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/analyzeCampaign`, state).then((res) => {
+        axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/analyzeCampaign/${state.language}`, state).then((res) => {
             setResult(res.data)
-        }).catch((err) => console.log(err)).finally(() => setLoading(false))
+        }).catch((err) => { console.log(err); setError(true) }).finally(() => setLoading(false))
     }
 
+    const handleCategoryChange = (e) => {
+        const newCatId = e.target.value;
+        const newCategory = categories.find(cat => parseInt(cat.id) === parseInt(newCatId)).name
+        setState({ ...state, category: newCategory, categoryId: newCatId })
+    }
     return (
         <div style={{ padding: "20px 40px" }}>
             <Grid container direction="column">
                 <Typography variant="h3" style={{ marginBottom: 20 }}>{"Analyze a Potential Campaign"}</Typography>
                 <Typography variant="subtitle1">We've developed a model to indicate how well your GoFundMe campaign would do based on several criteria</Typography>
                 <Typography variant="subtitle1">Fill in the following fields and click submit to see your results</Typography>
-                <Typography variant="subtitle2" style={{ marginTop: 20 }}>The data used to make this prediction was in English and Italian - this prediction will only be accurate using one of those two languages</Typography>
                 <Card style={{ backgroundColor: "#deeaf7", marginTop: 30, maxWidth: 1000 }}>
                     <CardContent >
                         <form onSubmit={(e) => handleSubmit(e)}>
@@ -65,11 +93,11 @@ const CampaignAnalyze = (props) => {
                                     </TextField>
                                 </Grid>
                                 <Grid item xs={6}>
-                                    <TextField required value={state.category} onChange={(e) => setState({ ...state, category: e.target.value })} select SelectProps={{ native: true }} style={{ backgroundColor: "white", borderRadius: 4 }} fullWidth variant="outlined" label="Category" >
+                                    <TextField required value={state.categoryId} onChange={(e) => handleCategoryChange(e)} select SelectProps={{ native: true }} style={{ backgroundColor: "white", borderRadius: 4 }} fullWidth variant="outlined" label="Category" >
                                         {/* the category field in the original DB was often "unknown" so we took the category id and looked in GoFundMe to get the real categories */}
                                         <option value="" />
                                         {categories.map((category, index) => (
-                                            <option key={index} value={category}>{category}</option>
+                                            <option key={index} value={category.id}>{category.name}</option>
                                         ))}
                                     </TextField>
                                 </Grid>
@@ -114,6 +142,17 @@ const CampaignAnalyze = (props) => {
                                         }
                                         label="The campaign will be searchable"
                                     />
+                                    <FormControlLabel
+                                        style={{ marginBottom: 20 }}
+                                        control={
+                                            <Checkbox
+                                                checked={state.hasBeneficiary}
+                                                onChange={(e) => setState({ ...state, hasBeneficiary: e.target.checked })}
+                                                name="isCharity"
+                                            />
+                                        }
+                                        label="The campaign will have a beneficiary"
+                                    />
 
                                 </Grid>
                                 <Grid item xs={6}>
@@ -131,6 +170,7 @@ const CampaignAnalyze = (props) => {
                                             }
                                                 label="Image" />
                                         </FormControl>
+                                        <Typography variant="subtitle2" style={{ marginTop: 20, marginBottom: 10 }}>The data used to make this prediction was in English and Italian - this prediction will only be accurate using one of those two languages</Typography>
                                         <TextField label="Language" variant="outlined" style={{ backgroundColor: "white", borderRadius: 4 }} required select SelectProps={{ native: true }} value={state.language} onChange={(e) => setState({ ...state, language: e.target.value })}>
                                             <option value="" />
                                             <option value="english">English</option>
@@ -138,14 +178,21 @@ const CampaignAnalyze = (props) => {
                                         </TextField>
                                     </Grid>
                                 </Grid>
-                                <Grid style={{ margin: 40 }} container justify="space-between">
+                                <Grid style={{ margin: 40 }} container justify="space-between" wrap="nowrap" alignItems="center" spacing={3}>
                                     {loading ? <Grid container justify="center"><CircularProgress color="secondary" /></Grid> :
-                                        <Button size="large" style={{ color: "white", padding: 20, fontSize: 24 }} variant="contained" color="secondary" type="submit">Analyze Campaign</Button>
+                                        <Button size="large" style={{ color: "white", padding: 20, fontSize: 24, marginRight: 20 }} variant="contained" color="secondary" type="submit">Analyze Campaign</Button>
                                     }
                                     <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
                                         {result && <CheckCircle className="final-check-symbol" color="secondary" style={{ fontSize: 40 }} />}
-                                        {result && <Typography style={{ marginBottom: 20 }} variant="h4" color="secondary">{result}%</Typography>}
-                                        {result && <Typography variant="body1">This means that we expect you to gain {parseFloat(result).toFixed(3)}% of your goal each day.</Typography>}
+                                        {result && <Typography className="final-check-symbol" style={{ marginBottom: 20 }} variant="h4" color="secondary">{parseFloat(result).toFixed(4)}%</Typography>}
+                                        {result && <Typography style={{ maxWidth: 500 }} variant="body1">This means that we expect you to gain {parseFloat(result).toFixed(3)}% ({((parseFloat(result).toFixed(3) / 100) * state.goal).toFixed(2)} {state.currencyCode}) of your goal each day.
+                                        <br />
+                                            <br />
+                                            {parseFloat(result) > 0 ? `It would take you ${Math.ceil(1 / (parseFloat(result) / 100))} day${Math.ceil(1 / (parseFloat(result) / 100)) > 1 ? "s" : ""} to reach your goal.` : "We don't expect you would reach your goal."}
+
+                                        </Typography>
+                                        }
+                                        {error && <Typography variant="body1" style={{ color: "red" }}>{"An unexpected error occurred or an input was invalid, please ensure all inputs are correct and try again."}</Typography>}
                                     </div>
                                 </Grid>
                             </Grid>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,9 @@ const Footer = (props) => {
             <Grid container justify="space-around">
                 <Grid item xs={12}>
                     <Typography style={{ color: "white" }}>
-                        AnalyzeFundMe is an educational tool built by BYU Information Systems students and is in no way affiliated with GoFundMe.
-                        {/* We are in no way affiliated with GoFundMe and merely developed this tool for educational purposes.*/}
-                        </Typography>
+                        {"AnalyzeFundMe is an educational tool built by BYU Information Systems students and is in no way affiliated with GoFundMe."}
+                    </Typography>
                 </Grid>
-                {/* <Grid item xs={6}>
-                    <Typography style={{ color: "white" }}>We are in no way affiliated with GoFundMe and merely developed this tool for educational purposes.</Typography>
-                </Grid> */}
             </Grid>
         </div>
     )
