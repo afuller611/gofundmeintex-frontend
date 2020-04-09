7a189ac8d4aa6afe979db531f9f1f49695a25ba4
DallenM <56405642+DallenM@users.noreply.github.com>
2020-04-09 10:23:35 -0600

    Changed Footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,10 @@ const Footer = (props) => {
         <div style={{ backgroundColor: "#333232", padding: "50px 30px", minHeight: 50 }}>
             <Grid container justify="space-around">
                 <Grid item xs={12}>
-                    <Typography style={{ color: "white" }}>We are in no way affiliated with GoFundMe and merely developed this tool for educational purposes.</Typography>
+                    <Typography style={{ color: "white" }}>
+                        AnalyzeFundMe is an educational tool built by BYU Information Systems students and is in no way affiliated with GoFundMe.
+                        {/* We are in no way affiliated with GoFundMe and merely developed this tool for educational purposes.*/}
+                        </Typography>
                 </Grid>
                 {/* <Grid item xs={6}>
                     <Typography style={{ color: "white" }}>We are in no way affiliated with GoFundMe and merely developed this tool for educational purposes.</Typography>
